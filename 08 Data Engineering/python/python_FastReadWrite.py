79fac5225a4ba6c68357ff8b4513b6a9083aed32
Geoffrey Link <geoffrey.link@gmail.com>
2019-02-01 14:20:07 -0500

    added python fast read and write

---
+++ 08 Data Engineering/python/python_FastReadWrite.py
@@ -0,0 +1,12 @@
+import os
+import boto3
+
+s3 = boto3.resource('s3')
+
+bucket = s3.Bucket('amazon-reviews-pds')
+
+for obj in bucket.objects.all():
+  if obj.key[:3] == 'tsv' and len(obj.key) > 4:
+    cmd = 'aws s3 cp s3://amazon-reviews-pds/' + obj.key + ' .'
+    print cmd
+    os.system(cmd)
