6223b6786bcd8ddc8832adac0e7f593ae922c6db
Geoffrey Link <geoffrey.link@gmail.com>
2019-02-02 09:31:22 -0500

    correct script

--- 08 Data Engineering/python/python_LineByLine.py
+++ 08 Data Engineering/python/python_LineByLine.py
@@ -10,7 +10,7 @@ bucket = s3.Bucket('amazon-reviews-pds')
 for obj in bucket.objects.all():
   if obj.key[:3] == 'tsv' and len(obj.key) > 4:
     cmd = 'aws s3 cp s3://amazon-reviews-pds/' + obj.key + ' /home/ec2-user/'
-#    os.system(cmd)
+    os.system(cmd)
 
 cmd = 'gunzip /home/ec2-user/*.gz'
 os.system(cmd)
