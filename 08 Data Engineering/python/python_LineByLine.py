9f26002ab57c242c4b8f46ebed7d3e19aa4eadfd
Geoffrey Link <geoffrey.link@gmail.com>
2019-02-02 09:30:24 -0500

    correct script

--- 08 Data Engineering/python/python_LineByLine.py
+++ 08 Data Engineering/python/python_LineByLine.py
@@ -10,7 +10,7 @@ bucket = s3.Bucket('amazon-reviews-pds')
 for obj in bucket.objects.all():
   if obj.key[:3] == 'tsv' and len(obj.key) > 4:
     cmd = 'aws s3 cp s3://amazon-reviews-pds/' + obj.key + ' /home/ec2-user/'
-    os.system(cmd)
+#    os.system(cmd)
 
 cmd = 'gunzip /home/ec2-user/*.gz'
 os.system(cmd)
@@ -47,14 +47,22 @@ for root, dirs, files in os.walk('/home/ec2-user/'):
 
         if line_to_read:
 
+        while line_count < 100000:
+
+          file_to_write.write(line_manipulation(line_to_read))
+          line_count  += 1
+          line_to_read = file_to_read.readline()
+
+        if line_to_read:
+
           line_count    = 0
           file_count   += 1
           file_number   = "%05d" % file_count
           file_to_write.close()
           file_to_write = open('/home/ec2-user/text_' + file_number + '.txt', 'a')
 
-    file_to_write.close()
-    file_to_read.close()
+      file_to_write.close()
+      file_to_read.close()
 
 end = time.time()
 print(end - start)/60
