93f24c37a74ac13cdd208d24d3e630cc06816c1b
Geoffrey Link <geoffrey.link@gmail.com>
2019-02-03 10:01:54 -0500

    added ngram

---
+++ 06 Machine Learning/SparkML/sparkML_ngram.py
@@ -0,0 +1,16 @@
+from pyspark.ml.feature import NGram
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+wordDataFrame = spark.createDataFrame([
+                                       (0, ["Hi", "I", "heard", "about", "Spark"]),
+                                       (1, ["I", "wish", "Java", "could", "use", "case", "classes"]),
+                                       (2, ["Logistic", "regression", "models", "are", "neat"])
+                                      ], ["id", "words"])
+
+ngram          = NGram(n=2, inputCol="words", outputCol="ngrams")
+ngramDataFrame = ngram.transform(wordDataFrame)
+ngramDataFrame.select("ngrams").show(truncate=False)
+
+spark.stop()
