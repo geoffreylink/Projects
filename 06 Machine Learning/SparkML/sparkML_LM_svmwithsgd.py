7b19dea0b10ff4854688923af4d6728bf3c850e6
Geoffrey Link <geoffrey.link@gmail.com>
2019-02-04 13:12:43 -0500

    updated svmwithsgd

--- 06 Machine Learning/SparkML/sparkML_LM_svmwithsgd.py
+++ 06 Machine Learning/SparkML/sparkML_LM_svmwithsgd.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext
 from pyspark.mllib.classification import SVMWithSGD, SVMModel
 from pyspark.mllib.regression import LabeledPoint
 
-sc = SparkContext(appName="PythonSVMWithSGDExample")
+sc = SparkContext()
 sc.setLogLevel('ERROR')
 
 def parsePoint(line):
@@ -15,5 +15,6 @@ parsedData = data.map(parsePoint)
 model = SVMWithSGD.train(parsedData, iterations=100)
 
 labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
-trainErr = labelsAndPreds.filter(lambda lp: lp[0] != lp[1]).count() / float(parsedData.count())
+trainErr       = labelsAndPreds.filter(lambda lp: lp[0] != lp[1]).count() / float(parsedData.count())
+
 print("Training Error = " + str(trainErr))
