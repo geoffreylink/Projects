27e95b5d339e7010ce6da2a7073f4c9fd4bac359
Geoffrey Link <geoffrey.link@gmail.com>
2019-02-07 22:03:25 -0500

    updated python_pywren

--- 08 Data Engineering/pywren/python_pywren.py
+++ 08 Data Engineering/pywren/python_pywren.py
@@ -1,7 +1,8 @@
+# t2.micro, 100Gb gp2 volume
+
 import os
 import sys
 import boto3
-import pandas as pd
 
 s3     = boto3.resource('s3')
 bucket = s3.Bucket('amazon-reviews-pds')
@@ -9,11 +10,16 @@ bucket = s3.Bucket('amazon-reviews-pds')
 for obj in bucket.objects.all():
   if obj.key[:3] == 'tsv' and len(obj.key) > 4:
     cmd = 'aws s3 cp s3://amazon-reviews-pds/' + obj.key + ' /home/ec2-user/'
-    print cmd
-    os.system(cmd)
+#    os.system(cmd)
 
-#s3.Object('pywren1', 'amazon_reviews_us_Wireless_v1_00.tsv').upload_file(Filename='amazon_reviews_us_Wireless_v1_00.tsv')
-#s3.Object('pywren1', 'amazon_reviews_us_Wireless_v1_00.tsv').download_file('/home/ec2-user/homer.txt')
+cmd = 'gunzip /home/ec2-user/*.gz'
+#os.system(cmd)
+
+for root, dirs, files in os.walk('/home/ec2-user/'):
+  for filename in files:
+    if filename[-3:] == 'tsv':
+      continue
+#      s3.Object('pywren1', filename).upload_file(Filename=filename)
 
 def line_manipulation(line_to_manipulate):
 
@@ -21,44 +27,52 @@ def line_manipulation(line_to_manipulate):
 
   return line_to_manipulate
 
-obj        = s3.Object('pywren1', 'amazon_reviews_us_Wireless_v1_00.tsv')
-body       = obj.get()['Body']
-blocksize  = 1024*1024
-buf        = body.read(blocksize)
+bucket     = s3.Bucket('pywren1')
 build_file = ''
 file_count = 0
 line_count = 0
 
-while len(buf) > 0:
+for obj in bucket.objects.all():
 
-  lines = buf.split('\n')
+  print obj.key
 
-  for line in lines:
+  obj       = s3.Object('pywren1', obj.key)
+  body      = obj.get()['Body']
+  blocksize = 1024*1024
+  buf       = body.read(blocksize)
 
-    if line.count('\t') > 13:
+  while len(buf) > 0:
 
-      if line_count < 100000:
+    lines = buf.split('\n')
 
-        line_count += 1
-        build_file = build_file + line_manipulation(line)
+    for line in lines:
 
-      else:
+      if line.count('\t') > 13:
 
-        file_number   = "%05d" % file_count
-        file_to_write = open('/home/ec2-user/text_' + file_number + '.txt', 'a')
-        file_to_write.write(build_file)
-        file_to_write.close()
-        build_file  = ''
-        line_count  = 0
-        file_count += 1
+        if line_count < 99999:
 
-  if line.count('\t') > 13:
-    buf = body.read(blocksize)
-  else:
+          line_count   += 1
+          build_file    = build_file + line_manipulation(line)
+
+        else:
+
+          build_file    = build_file + line_manipulation(line)
+          file_number   = "%05d" % file_count
+          file_to_write = open('/home/ec2-user/text_' + file_number + '.txt', 'a')
+          file_to_write.write(build_file)
+          file_to_write.close()
+          build_file    = ''
+          line_count    = 0
+          file_count   += 1
+
     buf = line + body.read(blocksize)
 
-if line_count > 0:
-  file_number   = "%05d" % file_count
-  file_to_write = open('/home/ec2-user/text_' + file_number + '.txt', 'a')
-  file_to_write.write(build_file)
-  file_to_write.close()
+  if line_count > 0:
+    file_number   = "%05d" % file_count
+    file_to_write = open('/home/ec2-user/text_' + file_number + '.txt', 'a')
+    file_to_write.write(build_file)
+    file_to_write.close()
+    build_file    = ''
+    line_count    = 0
+    file_count   += 1
+
